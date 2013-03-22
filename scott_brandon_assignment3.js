fd12663bc963e6ea7c543cce5759fc6e5ee24d08
b20973 <iamlee@fullsail.edu>
2013-03-21 20:26:12 -0400

    Added Global Variables

---
+++ scott_brandon_assignment3.js
@@ -0,0 +1,25 @@
+/* Brandon Lee Scott
+March 19, 2013
+Deliverable 3
+"Cleveland International Film Festival"
+*/
+
+//alert("JavaScript works!");
+
+//Global Variables
+
+var movieType1 = "comedy",
+	movieType2 = "action",
+	movieType3 = "drama",
+	actionFilm = '"180 Seconds"',
+	comedyFilm = '"Chasing Rainbows"',
+	dramaFilm = "Blondie",
+	destination1 = "movies",
+	destination2 = "dinner",
+	destination3 = "hookah bar",
+	thisWeekend = "Saturday",
+	cousin = "Kei",
+	numberOfPeople = 4,
+	thisSaturday = true,
+	thisFriday = false;
+
