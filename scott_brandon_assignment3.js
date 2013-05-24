208fb3cd1c0234668a5074c63d2c1ea4b5ac910d
b20973 <iamlee@fullsail.edu>
2013-05-23 23:14:50 -0400

    method accessor and boolean fuction

--- scott_brandon_assignment3.js
+++ scott_brandon_assignment3.js
@@ -11,3 +11,34 @@ var usualAmount = 25,//usual amount to save.
 	toMany="I have to many bills this week to put my usual amount of $"+usualAmount
 ;
 
+var jobs = {
+	nextWeek:"I don\'t have enough for this week.",
+	paycheck1: 395,
+	paycheck2: null,
+	weeklyIncome: [],
+	//property boolean
+	enough: true,
+	//poperty array
+	enoughEachWeek: [],
+	hoursWorked: null,
+	//method mutator
+	setPaycheck2: function (hoursWorked, rate){
+			var check = hoursWorked*rate;
+			return jobs.paycheck2=check;
+	},
+	//method accessor
+	getHoursWorked: function (hoursWorked){
+		jobs.setPaycheck2(hoursWorked,12);
+	},
+};
+
+//booleanfunction
+var haveEnough = function (paycheck1){
+	if (paycheck1 >= 350) {
+		console.log("I can maybe save the usual amount of $"+usualAmount)
+		return enough = true;
+	} else{
+		return enough = false;
+	};
+};
+
