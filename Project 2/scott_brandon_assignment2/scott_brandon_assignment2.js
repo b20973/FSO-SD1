aefd9154ff0619b714d38937cc1c9ecb881c1dbb
b20973 <iamlee@fullsail.edu>
2013-06-13 20:13:28 -0400

    Setting variables

--- Project 2/scott_brandon_assignment2/scott_brandon_assignment2.js
+++ Project 2/scott_brandon_assignment2/scott_brandon_assignment2.js
@@ -5,3 +5,10 @@ Project 2
 Saving enough for my next holiday.
 */
 
+var usualAmount = 25,//usual amount to save.
+	bills = [335,190,120,250], 
+	weeks = [1,2,3,4],
+	enoughEachWeek = [],
+	next ="I don\'t have enough for this week.",
+	toMany="I have to many bills this week to put my usual amount of $"+usualAmount
+;
