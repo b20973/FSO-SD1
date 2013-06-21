4e99b4199892921e4c97cef96b7b2f076bc20ca3
b20973 <iamlee@fullsail.edu>
2013-06-20 21:47:02 -0400

    JSON

---
+++ Project 3/json.js
@@ -0,0 +1,35 @@
+//Brandon Lee Scott
+//SDI-1305
+//JSON
+
+var json = {
+	"luggage": [
+		{
+			"piece": "luggage1",
+			"luggageTag": "Cathy",
+			"color": ["red","black"],
+			"weight": 58, 
+			"length": 50,
+			"oversized": true,
+			"inLuggage": "sun glasses"
+		},
+		{
+			"piece": "luggage2",
+			"luggageTag": "Brandon Lee",
+			"color": ["Gray","silver"],
+			"weight": 45,
+			"length": 32,
+			"oversized": false,
+			"inLuggage": "snorkle"
+		},
+		{
+			"piece": "luggage3",
+			"luggageTag": "Lennon",
+			"color": ["purple","pink", "blue"],
+			"weight": 60,
+			"length": 24,
+			"oversized": false,
+			"inLuggage": "bathing suit"
+		}
+	]
+};
