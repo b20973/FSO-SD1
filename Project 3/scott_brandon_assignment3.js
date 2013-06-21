a7fba4d574f389c3856b437ed6b6a0047e56b532
b20973 <iamlee@fullsail.edu>
2013-06-20 21:35:29 -0400

    constructor/local variable

---
+++ Project 3/scott_brandon_assignment3.js
@@ -0,0 +1,13 @@
+//Brandon Lee Scott
+//SDI 1305 
+//Assignment 3
+//
+
+
+var passengerList= [];
+//var takeTrip = 
+
+//Constructor
+var passenger = function (name,age,gender) {
+	//Local Variable
+	var luggageItems = [];
